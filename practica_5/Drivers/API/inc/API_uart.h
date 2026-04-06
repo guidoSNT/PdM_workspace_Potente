fc32779cddbb1cbdea37c30ededca110c5a59b16
Guido Potente <guidopotente02@gmail.com>
2026-04-06 11:13:31 -0300

    Feat: added SendString function

--- practica_5/Drivers/API/inc/API_uart.h
+++ practica_5/Drivers/API/inc/API_uart.h
@@ -3,20 +3,20 @@
 #include "API_delay.h" // for the bool_t
 #include "stm32f4xx_hal.h"
 
-#define API_UART_INSTANCE USART2           //! USART instance to use
+#define API_UART_INSTANCE USART2 //! USART instance to use
 #define API_UART_INSTANCE_STR "USART2"
-#define API_UART_BUADRATE 115200           //! Buadrate in bd of the USART
-#define API_UART_WL UART_WORDLENGTH_8B     //! Wordlength of 8 bits
+#define API_UART_BUADRATE 115200       //! Buadrate in bd of the USART
+#define API_UART_WL UART_WORDLENGTH_8B //! Wordlength of 8 bits
 #define API_UART_WL_STR "UART_WORDLENGTH_8B"
-#define API_UART_SB UART_STOPBITS_1        //! one stop bit
+#define API_UART_SB UART_STOPBITS_1 //! one stop bit
 #define API_UART_SB_STR "UART_STOPBITS_1"
-#define API_UART_PARITY UART_PARITY_NONE   //! No parity bit
+#define API_UART_PARITY UART_PARITY_NONE //! No parity bit
 #define API_UART_PARITY_STR "UART_PARITY_NONE"
-#define API_UART_MODE UART_MODE_TX_RX      //! Mode to send and receive
+#define API_UART_MODE UART_MODE_TX_RX //! Mode to send and receive
 #define API_UART_MODE_STR "UART_MODE_TX_RX"
 #define API_UART_HWCTL UART_HWCONTROL_NONE //! No control flow
 #define API_UART_HWCTL_STR "UART_HWCONTROL_NONE"
-#define API_UART_OS UART_OVERSAMPLING_16   //! Oversampling enabled
+#define API_UART_OS UART_OVERSAMPLING_16 //! Oversampling enabled
 #define API_UART_OS_STR "UART_OVERSAMPLING_16"
 
 // This macros are needed to convert an macro value to string
@@ -28,7 +28,30 @@
  * @brief Initializes the UART and sends the config on success
  *
  * @retval true on success else false
- * */
+ */
 bool_t uartInit();
 
+/**
+ * @brief Sends NULL terminated string through UART (unsecure).
+ *
+ * Sends the NULL terminated string without checking for size. This means that
+ * none NULL terminated strings will do an out-of-bounds read (undefined
+ * behaviour).
+ *
+ * @param[in] pstring Pointer to the NULL terminated string to send.
+ *
+ * @retval None
+ */
+void uartSendString(uint8_t *pstring);
+
+/**
+ * @brief Sends NULL terminated string through UART.
+ *
+ * @param[in] pstring Pointer to the NULL terminated string to send.
+ * @param[in] size Size of the string to send with the NULL.
+ *
+ * @retval None
+ */
+void uartSendStringSize(uint8_t *pstring, uint16_t size);
+void uartReceiveStringSize(uint8_t *pstring, uint16_t size);
 #endif /* API_INC_API_UART_H_ */
--- practica_5/Drivers/API/src/API_uart.c
+++ practica_5/Drivers/API/src/API_uart.c
@@ -1,18 +1,18 @@
 #include "API_uart.h"
 
-UART_HandleTypeDef uart_handle;
+static UART_HandleTypeDef uart_handle; //! Handler for the HAL uart comms
+static
 
-bool_t uartInit() {
+    bool_t
+    uartInit() {
   // Config string (it's weird and has to be configured if uart_handle is
   // changed but it's better than using an printf with the raw values)
   char config[] = API_UART_INSTANCE_STR
-                  "|" MACRO_STR(API_UART_BUADRATE)
-                  "|" API_UART_WL_STR
-                  "|" API_UART_SB_STR
-                  "|" API_UART_PARITY_STR
-                  "|" API_UART_MODE_STR
-                  "|" API_UART_HWCTL_STR
-                  "|" API_UART_OS_STR;
+      "|" MACRO_STR(API_UART_BUADRATE) "|" API_UART_WL_STR "|" API_UART_SB_STR
+                                       "|" API_UART_PARITY_STR
+                                       "|" API_UART_MODE_STR
+                                       "|" API_UART_HWCTL_STR
+                                       "|" API_UART_OS_STR;
 
   uart_handle = (UART_HandleTypeDef){
       .Instance = API_UART_INSTANCE,
@@ -32,4 +32,18 @@ bool_t uartInit() {
   HAL_UART_Transmit(&uart_handle, (uint8_t *)config, sizeof(config),
                     HAL_MAX_DELAY);
   return true;
+}
+
+void uartSendString(uint8_t *pstring) {
+  size_t str_size = 0;
+  char *str_cp = (char *)pstring;
+  if (pstring == NULL)
+    return;
+
+  // If str_cp doesn't end with \0 this will read out of bounds
+  do {
+    str_size++;
+  } while (*str_cp++ != '\0');
+
+  HAL_UART_Transmit(&uart_handle, pstring, str_size, HAL_MAX_DELAY);
 }
